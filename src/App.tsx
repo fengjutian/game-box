9178a2cac79d56fc18c51f36f0f7f9594beeb3d7
fengjutian <2640111081@qq.com>
2025-03-09 18:57:04 +0800

    feat: save

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import "./App.css";
-import LionComp from "./gams/Lion/Lion.tsx";
+import { NavLink } from "react-router";
+// import LionComp from "./gams/Lion/Lion.tsx";
 
 function App() {
+  const goGame = (gameName: string)  => {
+
+
+  }
+
   return (
-    <main className="container">
-      首页
-      <LionComp/>
-    </main>
+    <div>
+      <h1>欢迎来到小游戏世界</h1>
+      <h2>点击下方游戏名称进入游戏</h2>
+      <div>
+        <div>
+          <NavLink to="/lion" end>
+            爱吹风的小狮子
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/king" end>
+            杀死国王
+          </NavLink>
+        </div>
+      </div>
+    </div>
   );
 }
 
